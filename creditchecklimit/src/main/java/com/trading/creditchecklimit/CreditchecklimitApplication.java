74c3a92b0815637175cf9d25a487ccf23718643a
Kadirturan <abdulkadir.turan@gmail.com>
2023-12-09 01:13:37 +0300

    delete unnecessary files

--- creditchecklimit/src/main/java/com/trading/creditchecklimit/CreditchecklimitApplication.java
+++ creditchecklimit/src/main/java/com/trading/creditchecklimit/CreditchecklimitApplication.java
@@ -2,27 +2,27 @@ package com.trading.creditchecklimit;
 
 import com.trading.creditchecklimit.data.InitialData;
 import com.trading.creditchecklimit.handler.QueueHandler;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreditchecklimitApplication {
+public class CreditchecklimitApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-
-		InitialData.loadFromFile();
-		//InitialData.loadInitialData();
-
-
-
-
-
-
 
 		SpringApplication.run(CreditchecklimitApplication.class, args);
 
 
+	}
 
+	/**
+	 * Initial data load and thread pool starts listen order wait queue
+	 * @param args incoming main method arguments
+	 */
+	@Override
+	public void run(String... args) {
+		InitialData.loadFromFile();
 		QueueHandler queueListener = QueueHandler.getQueueListener();
 		queueListener.processOrders();
 
